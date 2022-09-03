84fe8d399a1ffe29c749a99e98a19c49e91046f8
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-03 20:56:36 +0100

    added animation loop

---
+++ game.js
@@ -0,0 +1,19 @@
+let stop = false,
+  now,
+  lastFrame;
+
+(function startAnimating() {
+  lastFrame = window.performance.now();
+  animate();
+})();
+
+function animate(newtime) {
+  if (stop) return;
+  requestAnimationFrame(animate);
+  now = newtime;
+  const elapsed = now - lastFrame;
+  if (elapsed > settings.fpsInterval) {
+    lastFrame = now - (elapsed % settings.fpsInterval);
+    update();
+  }
+}
